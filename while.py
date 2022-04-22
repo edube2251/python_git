762bfa2697b800dc6d67d8120951101c8ebdd6f0
edube2251 <57388986+edube2251@users.noreply.github.com>
2022-04-22 18:09:09 +0200

    Add files via upload

---
+++ while.py
@@ -0,0 +1,8 @@
+# WHILE LOOP: execute a set of statements as long as a condition is true.
+x = 1
+while x < 6:
+    print(str(x)+ " is the value inside loop ")
+    print()
+    x += 1
+else:
+    print(str(x)+" is the value outside loop")
